c1f34720ce022d85fa25d344826d317f2cf9b836
krishnadev7 <krishnadevv12@gmail.com>
2023-01-12 22:47:55 +0530

    3rd commit: toggle state in navbar completed

--- tailwind-cloudapp/src/components/Navbar.jsx
+++ tailwind-cloudapp/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faX } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 function Navbar() {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => {
+    setNav(!nav);
+  };
   return (
     <>
       <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
@@ -23,10 +29,27 @@ function Navbar() {
             </button>
             <button className='px-8 py-3'>Sign up</button>
           </div>
-          <div className='md:hidden'>
-            <FontAwesomeIcon icon={faBars} className='w-5' />
+          <div className='md:hidden' onClick={handleClick}>
+            {!nav ? (
+              <FontAwesomeIcon icon={faBars} className='w-5' />
+            ) : (
+              <FontAwesomeIcon icon={faX} className='w-5' />
+            )}
           </div>
         </div>
+        <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
+          <li className='border-b-2 border-zinc-300 w-full'>Home</li>
+          <li className='border-b-2 border-zinc-300 w-full'>About</li>
+          <li className='border-b-2 border-zinc-300 w-full'>Support</li>
+          <li className='border-b-2 border-zinc-300 w-full'>Platform</li>
+          <li className='border-b-2 border-zinc-300 w-full'>Pricing</li>
+          <div className='flex flex-col my-4'>
+            <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>
+              Sign In
+            </button>
+            <button className='px-8 py-3'>Sign Up</button>
+          </div>
+        </ul>
       </div>
     </>
   );
