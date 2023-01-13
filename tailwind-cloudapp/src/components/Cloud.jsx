b9174c3160374874b51918cc22f0c5dc31a57304
krishnadev7 <krishnadevv12@gmail.com>
2023-01-13 23:33:36 +0530

    5th commit: image and Details section added

--- tailwind-cloudapp/src/components/Cloud.jsx
+++ tailwind-cloudapp/src/components/Cloud.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import bgImg from '../assests/cyber-bg.png'
 
 function Cloud() {
   return (
@@ -8,7 +9,19 @@ function Cloud() {
                 <p className='text-2xl'>Unique Sequencing & Production</p>
                 <p className='py-3 text-5xl md:text-7xl font-bold'>Cloud Management</p>
                 <p className='text-2xl'>This is our Tech Brand.</p>
-                <button className='py-3 px-6 sm:w-[60%]'>Get started</button>
+                <button className='py-3 px-6 sm:w-[60%] my-4'>Get started</button>
+            </div>
+            <div>
+              <img className='w-full' src={bgImg} alt="background image" />
+            </div>
+            <div className='absolute flex flex-col py-8 md:min-w-[760px] bottom-[-5%] mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200 border-border-slate-300 rounded-xl text-center shadow-xl'>
+              <p>Data Services</p>
+              <div>
+                <p>App Securtity</p>
+                <p>Dashboard Design</p>
+                <p>Cloud Management</p>
+                <p>API</p>
+              </div>
             </div>
         </div>
     </div>
