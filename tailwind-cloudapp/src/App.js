379e1b24683c703370e6c5f3d7f73ad792dd92eb
krishnadev7 <krishnadevv12@gmail.com>
2023-01-21 23:12:18 +0530

    10th commit: Pricing page completed

--- tailwind-cloudapp/src/App.js
+++ tailwind-cloudapp/src/App.js
@@ -2,6 +2,7 @@ import About from './components/About';
 import AllInOne from './components/AllInOne';
 import Cloud from './components/Cloud';
 import Navbar from './components/Navbar';
+import Pricing from './components/Pricing';
 import Support from './components/Support';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <About />
       <Support />
       <AllInOne/>
+      <Pricing/>
     </>
   );
 }
